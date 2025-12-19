4fbe5a38bacfb5a0234822a06eb06701526b72a4
TroshinM <maksimtroshin18@yandex.com>
2025-12-19 12:30:53 +0400

    Check

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tea (sort, kind, additives, manufacturer, weight, price, image_url) VALUES
+('Зеленый', 'Листовой', 'Жасмин', 'Чайная империя', 100, 450.00, 'https://avatars.mds.yandex.net/get-mpic/5220759/img_id7198082708560111502.jpeg/orig'),
+('Черный', 'Листовой', 'Без добавок', 'Чайная империя', 200, 600.00, 'https://avatars.mds.yandex.net/get-mpic/11477103/2a0000018d01b9233c5343da4ec6ec2767da/orig'),
+('Белый', 'Листовой', 'Цветы апельсина', 'Гора Ми', 150, 800.00, 'https://yastatic.net/naydex/yandex-search/osBpF8688/6a4c6f2PqX/WXFklQOm33-S4UxND5i5WYfSVFBHIDdnwOLoWxegZ_zIG30ELu3FE7dUPM8GdGAy7WC-n7bPqElr2hvoloX4wx0jIsdB7dx7m_MFUR3bqVSKkVhGFWFjaZrZiikygVyFojYMKFX_112vT2rDVHItK_kmh_c'),
+('Пуэр', 'Прессованный', 'Без добавок', 'Yunnan Tea Co.', 350, 1200.00, 'https://img.joomcdn.net/d21340ed90404cf014f8d10c8562bd4feb9ad084_original.jpeg'),
+('Улун', 'Гранулированный', 'Мятное масло', 'Тайваньский сад', 100, 750.00, 'https://yastatic.net/naydex/yandex-search/osBpF8634/6a4c6f2PqX/WXFklQOm33-S4UxND5i5WYfSVFBHIDdnwOLoCUHzZquWSC0EK76WS7NVOJ1WIjMw6DG5yefF-UQyixzoidrhwB4iJsZH-Jh_l_UHR0OCtUmSnkkTXTJva_bZlCkxnEyYuTgCL2z_1wT4V2XJVW97d_4Mhw'),
+('Травяной', 'Смесь', 'Ромашка, шиповник', 'Эко-чай', 250, 350.00, 'https://avatars.mds.yandex.net/get-mpic/5238263/img_id4373427502611766938.png/orig'),
+('Зеленый', 'Порошок', 'Матча', 'Японская чайная компания', 30, 1200.00, 'https://cdn1.ozone.ru/s3/multimedia-m/c600/6438063934.jpg'),
+('Черный', 'Пакетированный', 'Без добавок', 'Lipton', 125, 250.00, 'https://avatars.mds.yandex.net/get-mpic/4448948/img_id777261902381203853.jpeg/orig'),
+('Зеленый', 'Листовой', 'Лимон', 'Чайная империя', 100, 400.00, 'https://ir.ozone.ru/s3/multimedia-k/c600/6731501852.jpg'),
+('Фруктовый', 'Смесь', 'Яблоко, малина', 'Ароматный мир', 200, 300.00, 'https://ir.ozone.ru/s3/multimedia-d/6489727813.jpg');
